79184572e1fd6d5859ed8d9567b96f7fed306486
vikdw <vikasdwivedi172@yahoo.com>
2025-02-12 13:08:18 +0530

    adding groovy folder

---
+++ groovy/1test.groovy
@@ -0,0 +1,24 @@
+
+def name="Vikas"
+def lname="Dwivedi"
+
+println "$name $lname"
+
+byte b = 10
+// println byte.MAX_VALUE
+
+def s = 100
+println s
+
+def t = "abc"
+
+println t
+
+
+assert 1 +3 == 4
+
+def sum(a,b){
+    def c = a + b
+    println c
+}
+sum(2,3)
