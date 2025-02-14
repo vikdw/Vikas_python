0f8fb0dd44548a86887963804a66238d86723147
vikdw <vikasdwivedi172@yahoo.com>
2025-02-14 19:51:14 +0530

    adding pything code to create file.

---
+++ jenkins/test_python.py
@@ -0,0 +1,9 @@
+print("testing pythn from jekins pipeline. will create a file in /tmp folder")
+
+
+with open('/tmp/test_python.txt', 'w') as f:
+    f.write("testing python in jenking with creating this file.")
+
+print("file created please check /tmp folder.")
+          
+          
