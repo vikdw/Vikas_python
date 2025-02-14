a569985f3dbe6a74ea093d7821bd508cfa3ad484
vikdw <vikasdwivedi172@yahoo.com>
2025-02-14 20:21:12 +0530

    updating .py file to check if file already exist.

--- jenkins/test_python.py
+++ jenkins/test_python.py
@@ -1,9 +1,16 @@
+import os
+
+file= "/tmp/test_python.txt"
+
 print("testing pythn from jekins pipeline. will create a file in /tmp folder")
 
+if os.path.exists(file):
+    print("file already exist, will remove file.")
+    os.remove(file)
 
-with open('/tmp/test_python.txt', 'w') as f:
+with open(file, 'w') as f:
     f.write("testing python in jenking with creating this file.")
 
-print("file created please check /tmp folder.")
+print("file created please check /tmp folder.   ")
           
           
